57e81e7c3a1988e0c6a02a55e562de6563c4568f
亨睿 <henry.lx@alibaba-inc.com>
2018-05-09 12:31:51 +0800

    Update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
 import * as tf from '@tensorflow/tfjs';
 import 'babel-polyfill';
 
-import { generateDataSet } from './data/LinearDataSet';
-
 window.tf = tf;
-
-const { training, test } = generateDataSet();
-const { features, labels } = training.nextBatch(2);
-
